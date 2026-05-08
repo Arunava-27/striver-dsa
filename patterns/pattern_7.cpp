599dd9f9bf2292f6f5291d90096391dfca9f7e87
Arunava-27 <drarunkundu22@gmail.com>
2026-05-08 20:20:24 +0530

    Refactor pattern_7.cpp for improved formatting and logic correction

--- patterns/pattern_7.cpp
+++ patterns/pattern_7.cpp
@@ -1,16 +1,25 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     cout << "Enter Range : ";
     cin >> n;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2*i-1; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
     }
-    
+
     return 0;
 }
