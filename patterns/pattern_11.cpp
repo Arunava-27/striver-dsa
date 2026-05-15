0262d3b5faab14643ebe81336846df09ee136dd1
Arunava-27 <drarunkundu22@gmail.com>
2026-05-15 16:36:44 +0530

    Refactor pattern_10.cpp for improved formatting and add pattern_11.cpp for binary pattern generation

--- patterns/pattern_11.cpp
+++ patterns/pattern_11.cpp
@@ -6,20 +6,23 @@ int main()
 {
     int n;
     cin >> n;
+
+    int sp = 1;
+
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= i; j++)
         {
-            cout << "*";
-        }
-        cout << endl;
-    }
-
-    for (int i = 1; i <= n - 1; i++)
-    {
-        for (int j = i; j <= n - 1; j++)
-        {
-            cout << "*";
+            cout << sp;
+            
+            if (sp == 1)
+            {
+                sp = 0;
+            }
+            else
+            {
+                sp = 1;
+            }
         }
         cout << endl;
     }
