1ebd4548b964c4de55dbc8a41f02a156fc80eb5d
Arunava-27 <drarunkundu22@gmail.com>
2026-06-09 12:42:20 +0530

    Format pattern_12.cpp for improved readability and consistency

--- patterns/pattern_12.cpp
+++ patterns/pattern_12.cpp
@@ -1,10 +1,33 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     cin >> n;
-    
+
+    for (int i = 1; i <= n; i++)
+    {
+        // left
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        // spaces
+        for (int k = 1; k <= 2 * (n - i); k++)
+        {
+            cout << " ";
+        }
+        // right
+        for (int l = i; l >= 1; l--)
+        {
+
+            cout << l;
+        }
+
+        cout << endl;
+    }
+
     return 0;
 }
