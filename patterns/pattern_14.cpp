538d2790251f932c23fb103e122128507d0837ec
Arunava-27 <drarunkundu22@gmail.com>
2026-06-16 17:29:32 +0530

    Add pattern_14.cpp for character pattern generation

---
+++ patterns/pattern_14.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int t = 65;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << char(t);            
+            // if (j < i)
+            // {
+            //     cout << " ";
+            // }
+            t++;
+        }
+        t=65;
+        cout << endl;
+        
+    }
+    
+    return 0;
+}
