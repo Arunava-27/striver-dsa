db22af4397b508846b0d529772f4c23dc3224d88
Arunava-27 <drarunkundu22@gmail.com>
2026-07-26 17:29:14 +0530

    Refactor pattern_21.java for improved readability and formatting

--- java/pattern_21.java
+++ java/pattern_21.java
@@ -8,25 +8,23 @@
  * @author arunava
  */
 
-// Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
-
-
+// Given an integer n. You need to recreate the pattern given below for any
+// value of N. Let's say for N = 5, the pattern should look like as below:
 
 // *****
-// *   *
-// *   *
-// *   *
+// * *
+// * *
+// * *
 // *****
-
 
 public class pattern_21 {
     public static void main(String[] args) {
-        int n=2;
+        int n = 2;
 
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                if(i>=2 && i<=n-1){
-                    if(j>=2 && j<=n-1){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i >= 2 && i <= n - 1) {
+                    if (j >= 2 && j <= n - 1) {
                         System.out.print(" ");
                     } else {
                         System.out.print("*");
