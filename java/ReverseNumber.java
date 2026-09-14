6e137f852997380b22f23bd0a5515b231eac2d27
Arunava-27 <drarunkundu22@gmail.com>
2026-09-14 16:06:53 +0530

    Add ReverseNumber.java to implement integer reversal functionality

---
+++ java/ReverseNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        int reversed = 0;
+        while (n != 0) {
+            int digit = n % 10;
+            reversed = reversed * 10 + digit;
+            n /= 10;
+        }
+        System.out.println(reversed);
+    }
+}
